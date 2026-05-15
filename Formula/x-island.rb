0725ce12202585d4973344791a26053605b3d230
Meteorkid <2838801110@qq.com>
2026-05-15 18:43:16 +0800

    chore: update sha256 & fix DMG variable ordering

--- Formula/x-island.rb
+++ Formula/x-island.rb
@@ -1,6 +1,6 @@
 cask "x-island" do
   version "1.3.5"
-  sha256 :no_check
+  sha256 "cc8736920f4058fe6d4b157cc3b60ef13088e62091909e8c15ccdec8df3e7788"
 
   url "https://github.com/Meteorkid/XIsland/releases/download/v#{version}/XIsland-#{version}.dmg",
       verified: "github.com/Meteorkid/XIsland/"
