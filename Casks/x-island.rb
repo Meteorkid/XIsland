3041904d6051fa956f80edc15a155d07c351321f
Meteorkid <2838801110@qq.com>
2026-08-01 02:20:29 +0800

    release: v1.12.0 - 灵动岛高度无极调节
    - 更新 VERSION 至 1.12.0
    - 更新 Cask sha256

--- Casks/x-island.rb
+++ Casks/x-island.rb
@@ -1,8 +1,8 @@
 # 注意：版本号应与 VERSION 文件保持一致
 # 更新步骤：1. 修改 VERSION 文件 2. 更新此文件的 version 和 sha256
 cask "x-island" do
-  version "1.11.0"
-  sha256 "87d25c78c5769a604885c4e77d30259195a882969845ca6eb85066c40f132687"
+  version "1.12.0"
+  sha256 "5f8f69fb08625ab3facd635afec6f523e5edd144a5a92a9836c24e2f884a5d38"
 
   url "https://github.com/Meteorkid/XIsland/releases/download/v#{version}/XIsland-#{version}.dmg",
       verified: "github.com/Meteorkid/XIsland/"
